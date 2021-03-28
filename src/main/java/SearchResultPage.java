73289e95bc633335569c511cd7bb57b2c0de1f6a
Kirill Reuka <reuka.kirill@gmail.com>
2021-03-28 21:39:42 +0300

    Added 3 test case(not full. Need to add Asserts)

--- src/main/java/SearchResultPage.java
+++ src/main/java/SearchResultPage.java
@@ -5,6 +5,8 @@ public class SearchResultPage {
     WebDriver driver;
 
     private By firstHeartProduct = By.xpath("//button[@class='wish-button js-wish-button']");
+    private By searchField = By.xpath("//input[@name='search']");
+    private By searchButton = By.xpath("//button[text()=' Найти ']");
 
     public SearchResultPage(WebDriver driver) {
         this.driver = driver;
@@ -13,5 +15,11 @@ public class SearchResultPage {
     public SearchResultPage addToWishList(){
         driver.findElement(firstHeartProduct).click();
         return this;
+    }
+
+    public SearchResultPage search(String searchQuery) {
+        driver.findElement(searchField).sendKeys(searchQuery);
+        driver.findElement(searchButton).click();
+        return new SearchResultPage(driver);
     }
 }
--- src/test/java/WishListPageTest.java
+++ src/test/java/WishListPageTest.java
@@ -6,7 +6,7 @@ public class WishListPageTest extends BaseTest{
     private By heartIcon = By.xpath("//rz-wishlist[@class='header-actions__component']/a[@href='https://rozetka.com.ua/cabinet/wishlist/']");
 
     @Test
-        public void deleteOneItem(){
+    public void deleteOneItem(){
         HomePage homePage = new HomePage(driver);
         WishListPage wishListPage = new WishListPage(driver);
         SearchResultPage searchResultPage = new SearchResultPage(driver);
@@ -15,5 +15,18 @@ public class WishListPageTest extends BaseTest{
         searchResultPage.addToWishList();
         driver.findElement(heartIcon).click();
         wishListPage.deleteItemFromWishlist();
+    }
+
+    @Test
+    public void addSomeItems(){
+        HomePage homePage = new HomePage(driver);
+        WishListPage wishListPage = new WishListPage(driver);
+        SearchResultPage searchResultPage = new SearchResultPage(driver);
+        homePage.login("reuka.kirill@gmail.com", "Testpassword123");
+        homePage.search("Notebook");
+        searchResultPage.addToWishList();
+        searchResultPage.search("Mobile phone");
+        searchResultPage.addToWishList();
+        driver.findElement(heartIcon).click();
     }
 }
